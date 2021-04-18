1f23c15898979e54c2fe92d70b3bbde378469cbc
Carlos Pinelly <cpinelly@gmail.com>
2021-04-18 11:58:34 -0500

    walk and idle animations implemented.

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ screen = pygame.display.set_mode(WINDOWS_SIZE, 0, 32)
 
 display = pygame.Surface((600, 400))
 
-player_image = pygame.image.load('character.png')
 grass_image = pygame.image.load('grass.png')
 dirt_image = pygame.image.load('dirt.png')
 r_corner_image = pygame.image.load('right_corner.png')
@@ -35,6 +34,38 @@ def load_map(path):
 
     return game_map
 
+global animation_frames
+animation_frames = {}
+
+def load_animation(path, frame_durations):
+    global animation_frames
+    animation_name = path.split('/')[-1]
+    animation_frame_data = []
+    n = 1
+    for frame in frame_durations:
+        animation_frame_id = animation_name + '_' + str(n)
+        img_loc = path + '/' + animation_frame_id + '.png'
+        animation_image = pygame.image.load(img_loc)
+        animation_frames[animation_frame_id] = animation_image.copy()
+        for i in range(frame):
+            animation_frame_data.append(animation_frame_id)
+        n += 1
+
+    return animation_frame_data
+
+def change_action(action_var, frame, new_value):
+    if action_var != new_value:
+        action_var = new_value
+        frame = 0
+    return action_var, frame
+
+animation_database = {}
+animation_database['idle'] = load_animation('player_animations/idle', [10, 10, 10, 10])
+animation_database['walk'] = load_animation('player_animations/walk', [7, 7, 7, 7, 7, 7])
+
+player_action = 'idle'
+player_frame = 0
+player_flip = False
 
 game_map = load_map('map')
 
@@ -74,7 +105,7 @@ def move(rect: Rect, movement: Dict, tiles: List[Rect]) -> (Rect, Dict):
 
 player_y_momentun = 0
 
-player_rect = pygame.Rect(50, 50, player_image.get_width(), player_image.get_height())
+player_rect = pygame.Rect(50, 50, 26, 32)
 test_rect = pygame.Rect(100, 100, 100, 50)
 
 moving_right = False
@@ -130,6 +161,17 @@ while True:
     if player_y_momentun > 9:
         player_y_momentun = 9
 
+    if player_movement[0] > 0:
+        player_action, player_frame = change_action(player_action, player_frame, 'walk')
+        player_flip = False
+
+    if player_movement[0] == 0:
+        player_action, player_frame = change_action(player_action, player_frame, 'idle')
+
+    if player_movement[0] < 0:
+        player_action, player_frame = change_action(player_action, player_frame, 'walk')
+        player_flip = True
+
     player_rect, collisions = move(player_rect, player_movement, tile_rects)
 
     if collisions['bottom']:
@@ -141,7 +183,12 @@ while True:
     if collisions['top']:
         player_y_momentun = -player_y_momentun
 
-    display.blit(player_image, (player_rect.x - true_scroll[0], player_rect.y - true_scroll[1]))
+    player_frame += 1
+    if player_frame >= len(animation_database[player_action]):
+        player_frame = 0
+    player_img_id = animation_database[player_action][player_frame]
+    player_image = animation_frames[player_img_id]
+    display.blit(pygame.transform.flip(player_image, player_flip, False), (player_rect.x - true_scroll[0], player_rect.y - true_scroll[1]))
 
     for event in pygame.event.get():
         if event.type == QUIT:
