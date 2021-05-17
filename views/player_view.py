7cb8d1afe51cbe353251a5dfcaa1105f3c66b888
Carlos Pinelly <cpinelly@gmail.com>
2021-05-16 19:07:17 -0500

    Adjustment on player rendering.

--- views/player_view.py
+++ views/player_view.py
@@ -1,15 +1,20 @@
+from typing import Dict
 
 from config import ASSETS_FOLDER
 import pygame
+from pygame import Surface
 from engine.utils import Point
 from models import PlayerModel, PlayerActions
 
 class PlayerView(object):
+
+    _DEFAULT_PLAYER_SIZE = 32
+
     def __init__(self):
-        self.player_action = PlayerActions.IDLE
+        self.displaying_action = PlayerActions.IDLE
         self.player_frame = 0
         self.animation_database = {}
-        self.animation_frames = {}
+        self.animation_frames: Dict[str, Surface] = {}
         self.animation_database[PlayerActions.IDLE] = self._load_animation(ASSETS_FOLDER + 'characters/player/idle', [10, 10, 10, 10])
         self.animation_database[PlayerActions.WALK] = self._load_animation(ASSETS_FOLDER + 'characters/player/walk', [7, 7, 7, 7, 7, 7])
         self.animation_database[PlayerActions.JUMP] = self._load_animation(ASSETS_FOLDER + 'characters/player/jump', [5, 5, 7, 7])
@@ -33,8 +38,8 @@ class PlayerView(object):
         return animation_frame_data
 
     def _change_action(self, new_value):
-        if self.player_action != new_value:
-            self.player_action = new_value
+        if self.displaying_action != new_value:
+            self.displaying_action = new_value
             self.player_frame = 0
 
     def render(self, display: pygame.Surface, player: PlayerModel, scroll: Point):
@@ -46,5 +51,9 @@ class PlayerView(object):
 
         player_img_id = self.animation_database[player.action][self.player_frame]
         player_image = self.animation_frames[player_img_id]
+
+        # Sprites like those for jump action have height greater than 32, so we adjust wht height
+        position_adjustment = player_image.get_height() - self._DEFAULT_PLAYER_SIZE
+
         display.blit(pygame.transform.flip(player_image, not player.front_to_right, False),
-                     (player.x - scroll[0], player.y - scroll[1]))
+                     (player.x - scroll[0], player.y - scroll[1] - position_adjustment))
